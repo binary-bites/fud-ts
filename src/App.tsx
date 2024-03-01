88dd301b57d18263483dd62cbcefbe07ea522020
Herschenglime <herschenglime@gmail.com>
2024-03-01 10:34:09 -0500

    added DaisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button className="btn">DaisyUI button</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
