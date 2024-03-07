eaa409d3887d21d02d08c217df0f90932c350a31
Nishant Nagururu <nishantnvn05@gmail.com>
2024-03-06 19:36:36 -0500

    delete backend

--- run.sh
+++ run.sh
@@ -2,15 +2,9 @@
 
 # if bun installed, use that
 if [[ $(command -v bun) ]]; then
-    cd backend
-    bunx tsx watch server.ts &
-
     cd ..
     bun run dev -- --open
 else
-    cd backend
-    npx tsx watch server.ts &
-
     cd ..
     npx run dev -- --open
 fi
