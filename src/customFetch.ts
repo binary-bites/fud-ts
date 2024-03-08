0b95ad58342bc2bcd5f419037d225417c75ebb31
Nishant Nagururu <nishantnvn05@gmail.com>
2024-03-07 19:26:49 -0500

    changed fetch to ts

--- src/customFetch.ts
+++ src/customFetch.ts
@@ -1,12 +1,19 @@
-// Return a function from the hook that can be used to perform the fetch operation.
-export default async function customFetch(url, method, body, token) {
+// Define a type for the method argument
+type RequestMethod = 'GET' | 'POST' | 'PUT' | 'DELETE';
+
+// Optionally, define an interface for the body parameter if you expect a specific structure
+interface RequestBody {
+  [key: string]: any; // This allows any key with any value type, adjust according to your needs
+}
+
+export default async function customFetch(url: string, method: RequestMethod, body: RequestBody, token: string): Promise<Response | { data: null | Response; error: any }> {
   // Define headers
   const headers = new Headers({
     'Authorization': `Bearer ${token}`,
     'Content-Type': 'application/json',
   });
 
-  let response = null;
+  let response: Response | null = null;
   try {
     // Perform the fetch operation
     const fetchResponse = await fetch(url, {
@@ -14,7 +21,7 @@ export default async function customFetch(url, method, body, token) {
       headers: headers,
       body: JSON.stringify(body),
     });
-
+    console.log(fetchResponse)
     return fetchResponse;
   } catch (error) {
     console.error("Error in customFetch:", error);
@@ -25,14 +32,14 @@ export default async function customFetch(url, method, body, token) {
   }
 }
 
-export async function customGet(url, token) {
+export async function customGet(url: string, token: string): Promise<Response | { data: null | Response; error: any }> {
   // Define headers
   const headers = new Headers({
     'Authorization': `Bearer ${token}`,
     'Content-Type': 'application/json',
   });
 
-  let response = null;
+  let response: Response | null = null;
   try {
     // Perform the fetch operation
     const fetchResponse = await fetch(url, {
@@ -42,7 +49,7 @@ export async function customGet(url, token) {
 
     return fetchResponse;
   } catch (error) {
-    console.error("Error in customFetch:", error);
+    console.error("Error in customGet:", error);
     return {
       data: response, // In case of error, response might be null or contain error information if it was parsed
       error: error, // Contains the error object
