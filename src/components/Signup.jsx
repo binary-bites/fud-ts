7e6b04d020b0b6c34d01bc65187ce967e1bc1679
lcvoelker <lcvoelkerr@gmail.com>
2024-03-21 14:13:47 -0400

    background design

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -105,9 +105,9 @@ export default function Signup() {
   setLoading(false);
 }
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex items-center justify-center bg-indigo-100 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-        <Card className="shadow-md">
+        <Card className="shadow-md mt-4 bg-white" >
           <Card.Body>
             <div className="text-center">
               <h2 className="text-3xl font-extrabold text-gray-900">
