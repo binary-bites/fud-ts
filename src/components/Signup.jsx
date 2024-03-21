949871c6736dc8cd4be919f891f8527d88390cf7
Nishant Nagururu <nishantnvn05@gmail.com>
2024-03-21 16:49:25 -0400

    changed login and singup to daisy ui

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,4 @@
 import React, { useRef, useState } from "react";
-import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import customFetch from "../utils/customFetch";
@@ -104,138 +103,118 @@ export default function Signup() {
 
   setLoading(false);
 }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-indigo-100 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-        <Card className="shadow-md mt-4 bg-white" >
-          <Card.Body>
-            <div className="text-center">
-              <h2 className="text-3xl font-extrabold text-gray-900">
-                Sign Up
-              </h2>
+        <div className="card bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="text-3xl font-bold text-center">Sign Up</h2>
+            {error.general && <div className="alert alert-error">{error.general}</div>}
+            <form onSubmit={handleSubmit} className="space-y-6">
+              {/* Repeated form elements are refactored for TailwindCSS and DaisyUI */}
+              <div className="form-control">
+              <label className="label" htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                ref={emailRef}
+                required
+                className={`input input-bordered ${error.email ? 'input-error' : ''}`}
+              />
+              {error.email && <label className="label text-error">{error.email}</label>}
             </div>
-            {error.general && <Alert variant="danger">{error.general}</Alert>}
-            <Form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-              <Form.Group controlId="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  ref={emailRef}
-                  required
-                  className={`mt-1 p-2 block w-full rounded-md border ${
-                    error.email ? 'border-red-500' : 'border-gray-300'
-                  } shadow-sm focus:outline-none ${
-                    error.email ? 'focus:border-red-500 focus:ring-red-500' : 'focus:border-indigo-500 focus:ring-indigo-500'
-                  } focus:ring-opacity-50`}
-                />
-                {error.email && <p className="text-red-500 text-sm mt-1">{error.email}</p>}
-              </Form.Group>
 
-              <Form.Group controlId="username">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                  type="text"
-                  ref={usernameRef}
-                  required
-                  className={`mt-1 p-2 block w-full rounded-md border ${
-                    error.username ? 'border-red-500' : 'border-gray-300'
-                  } shadow-sm focus:outline-none ${
-                    error.username ? 'focus:border-red-500 focus:ring-red-500' : 'focus:border-indigo-500 focus:ring-indigo-500'
-                  } focus:ring-opacity-50`}
-                />
-                {error.username && <p className="text-red-500 text-sm mt-1">{error.username}</p>}
-              </Form.Group>
-              
-              <Form.Group controlId="firstName">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  ref={firstNameRef}
-                  required
-                  className={`mt-1 p-2 block w-full rounded-md border ${
-                    error.firstName ? 'border-red-500' : 'border-gray-300'
-                  } shadow-sm focus:outline-none ${
-                    error.firstName ? 'focus:border-red-500 focus:ring-red-500' : 'focus:border-indigo-500 focus:ring-indigo-500'
-                  } focus:ring-opacity-50`}
-                />
-                {error.firstName && <p className="text-red-500 text-sm mt-1">{error.firstName}</p>}
-              </Form.Group>
+            <div className="form-control">
+              <label className="label" htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                ref={usernameRef}
+                required
+                className={`input input-bordered ${error.username ? 'input-error' : ''}`}
+              />
+              {error.username && <label className="label text-error">{error.username}</label>}
+            </div>
 
-              <Form.Group controlId="lastName">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  ref={lastNameRef}
-                  required
-                  className={`mt-1 p-2 block w-full rounded-md border ${
-                    error.lastName ? 'border-red-500' : 'border-gray-300'
-                  } shadow-sm focus:outline-none ${
-                    error.lastName ? 'focus:border-red-500 focus:ring-red-500' : 'focus:border-indigo-500 focus:ring-indigo-500'
-                  } focus:ring-opacity-50`}
-                />
-                {error.lastName && <p className="text-red-500 text-sm mt-1">{error.lastName}</p>}
-              </Form.Group>
+            <div className="form-control">
+              <label className="label" htmlFor="firstName">First Name</label>
+              <input
+                type="text"
+                id="firstName"
+                ref={firstNameRef}
+                required
+                className={`input input-bordered ${error.firstName ? 'input-error' : ''}`}
+              />
+              {error.firstName && <label className="label text-error">{error.firstName}</label>}
+            </div>
 
-              <Form.Group controlId="dateOfBirth">
-                <Form.Label>Date of Birth</Form.Label>
-                <Form.Control
+            <div className="form-control">
+              <label className="label" htmlFor="lastName">Last Name</label>
+              <input
+                type="text"
+                id="lastName"
+                ref={lastNameRef}
+                required
+                className={`input input-bordered ${error.lastName ? 'input-error' : ''}`}
+              />
+              {error.lastName && <label className="label text-error">{error.lastName}</label>}
+            </div>
+            <div class="form-control w-full">
+                <label class="label" for="dateOfBirth">
+                  <span class="label-text">Date of Birth</span>
+                </label>
+                <input
                   type="date"
-                  ref={dateOfBirthRef}
+                  id="dateOfBirth"
+                  ref={dateOfBirthRef} // Use the ref object
                   required
-                  className={`mt-1 p-2 block w-full rounded-md border ${
-                    error.dateOfBirth ? 'border-red-500' : 'border-gray-300'
-                  } shadow-sm focus:outline-none ${
-                    error.dateOfBirth ? 'focus:border-red-500 focus:ring-red-500' : 'focus:border-indigo-500 focus:ring-indigo-500'
-                  } focus:ring-opacity-50`}
+                  className={`input input-bordered w-full mt-1 ${error.dateOfBirth ? 'input-error' : ''}`}
                 />
-                {error.dateOfBirth && <p className="text-red-500 text-sm mt-1">{error.dateOfBirth}</p>}
-              </Form.Group>
+                {error.dateOfBirth && <label className="label text-error">{error.dateOfBirth}</label>}
+              </div>
 
-              <Form.Group controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
+              <div class="form-control w-full">
+                <label class="label" for="password">
+                  <span class="label-text">Password</span>
+                </label>
+                <input
                   type="password"
-                  ref={passwordRef}
+                  id="password"
+                  ref={passwordRef} // Use the ref object
                   required
-                  className={`mt-1 p-2 block w-full rounded-md border ${
-                    error.password ? 'border-red-500' : 'border-gray-300'
-                  } shadow-sm focus:outline-none ${
-                    error.password ? 'focus:border-red-500 focus:ring-red-500' : 'focus:border-indigo-500 focus:ring-indigo-500'
-                  } focus:ring-opacity-50`}
+                  className={`input input-bordered w-full mt-1 ${error.password ? 'input-error' : ''}`}
                 />
-                {error.password && <p className="text-red-500 text-sm mt-1">{error.password}</p>}
-              </Form.Group>
+                {error.password && <label className="label text-error">{error.password}</label>}
+              </div>
 
-              <Form.Group controlId="password-confirm">
-                <Form.Label>Password Confirmation</Form.Label>
-                <Form.Control
+              <div class="form-control w-full">
+                <label class="label" for="password-confirm">
+                  <span class="label-text">Password Confirmation</span>
+                </label>
+                <input
                   type="password"
-                  ref={passwordConfirmRef}
+                  id="password-confirm"
+                  ref={passwordConfirmRef} // Use the ref object
                   required
-                  className={`mt-1 p-2 block w-full rounded-md border ${
-                    error.passwordConfirm ? 'border-red-500' : 'border-gray-300'
-                  } shadow-sm focus:outline-none ${
-                    error.passwordConfirm ? 'focus:border-red-500 focus:ring-red-500' : 'focus:border-indigo-500 focus:ring-indigo-500'
-                  } focus:ring-opacity-50`}
+                  className={`input input-bordered w-full mt-1 ${error.passwordConfirm ? 'input-error' : ''}`}
                 />
-                {error.passwordConfirm && <p className="text-red-500 text-sm mt-1">{error.passwordConfirm}</p>}
-              </Form.Group>
+                {error.passwordConfirm && <label className="label text-error">{error.passwordConfirm}</label>}
+              </div>
 
-              <Button
+              <button
                 disabled={loading}
                 type="submit"
-                className="mt-2 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="btn btn-primary w-full"
               >
                 {loading ? "Signing up..." : "Sign Up"}
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
+              </button>
+            </form>
+          </div>
+        </div>
         <div className="text-center mt-4">
           Already have an account?{" "}
-          <Link to="/login" className="text-indigo-600">
-            Log In
-          </Link>
+          <Link to="/login" className="link link-primary">Log In</Link>
         </div>
       </div>
     </div>
